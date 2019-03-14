90198fbc1ff242dd09b598a7b9894e86c57c53aa
Matthew Wright <matthewbwright@gmail.com>
2019-03-14 15:02:13 -0700

    translates message

--- coded_correspondence.py
+++ coded_correspondence.py
@@ -1,5 +1,21 @@
 # Hey there! How have you been? I've been great! I just learned about this really cool type of cipher called a  Caesar Cipher. Here's how it works: You take your message, something like "hello" and then you shift all of the letters by a certain offset. For example, if I chose an offset of 3 and a message of "hello", I would code my message by shifting each letter 3 places to the left (with respect to the alphabet). So "h" becomes "e", "e" becomes, "b", "l" becomes "i", and "o" becomes "l". Then I have my coded message,"ebiil"! Now I can send you my message and the offset and you can decode it. The best thing is that Julius Caesar himself used this cipher, that's why it's called the Caesar Cipher! Isn't that so cool! Okay, now I'm going to send you a longer coded message that you have to decode yourself!
 
-xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!
+# xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!
 
 # This message has an offset of 10. Can you decode it?
+
+# variables to hold the alphabet, punctuation, and message
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+punctuation = ".,?'! "
+message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
+
+# empty string for translated message
+translated_message = ""
+# iterate through each letter/punctuation in the string
+for letter in message:
+    if not letter in punctuation:
+        letter_value = alphabet.find(letter)
+        translated_message += alphabet[(letter_value + 10) % 26]
+    else:
+        translated_message += letter
+print(translated_message)
