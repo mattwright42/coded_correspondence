1c92eaef7a4f7a6f3e03c396470467ac1d5e6184
Matthew Wright <matthewbwright@gmail.com>
2019-03-14 15:14:46 -0700

    encodes message

--- coded_correspondence.py
+++ coded_correspondence.py
@@ -19,3 +19,14 @@ for letter in message:
     else:
         translated_message += letter
 print(translated_message)
+
+# encoding a message
+message_for_v = "hey vishal! This is a super cool cipher, thanks for showing me! What else you got?"
+translated_message = ""
+for letter in message_for_v:
+    if not letter in punctuation:
+        letter_value = alphabet.find(letter)
+        translated_message += alphabet[(letter_value - 14) % 26]
+    else:
+        translated_message += letter
+print(translated_message)
