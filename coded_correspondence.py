bb38c6a16d7d3afe7b8e5b55667fbf977fe91d01
Matthew Wright <matthewbwright@gmail.com>
2019-03-14 15:22:08 -0700

    adds decoding and encoding functions

--- coded_correspondence.py
+++ coded_correspondence.py
@@ -30,3 +30,30 @@ for letter in message_for_v:
     else:
         translated_message += letter
 print(translated_message)
+
+
+def decoder(message, offset):
+    translated_message = ""
+    for letter in message:
+        if not letter in punctuation:
+            letter_value = alphabet.find(letter)
+            translated_message += alphabet[(letter_value + offset) % 26]
+        else:
+            translated_message += letter
+    return translated_message
+
+
+def coder(message, offset):
+    translated_message = ""
+    for letter in message:
+        if not letter in punctuation:
+            letter_value = alphabet.find(letter)
+            translated_message += alphabet[(letter_value - offset) % 26]
+        else:
+            translated_message += letter
+    return translated_message
+
+
+message_one = "jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud."
+
+print(decoder(message_one, 10))
